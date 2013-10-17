8ce21d39f359b587b00d41caaebae8a1a643e011
scherrda <dahlia.scherr@gmail.com>
2013-10-17 22:36:52 +0200

    port for heroku

--- src/server.js
+++ src/server.js
@@ -29,8 +29,8 @@ function start() {
         response.end();
     }
 
-    http.createServer(onRequest).listen(9000);
-    console.log("Server has started at localhost:9000.");
+    http.createServer(onRequest).listen(process.env.PORT || 5000);
+    console.log("Server has started at localhost:5000.");
 }
 
 
